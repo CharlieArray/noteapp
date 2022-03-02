12522bf003a91aa9603d544f1f852bed7d1fe726
Charles <charles@getbackd.com>
2022-03-02 11:34:38 +0200

    [ADD]added query classes and auth/non-auth requests

--- schema.py
+++ schema.py
@@ -113,3 +113,39 @@ class deleteNote(graphene.Mutation):
         note = note
         session.commit()
         return deleteNote(ok=ok, note=note)
+
+class PostAuthMutation(graphene.ObjectType):
+    addNote = addNote.Field()
+    updateNote = updateNote.Field()
+    deleteNote = deleteNote.Field()
+
+
+class PreAuthMutation(graphene.ObjectType):
+    create_user = createUser.Field()
+
+class Query(graphene.ObjectType):
+    # what is happening?
+    # find single note
+    findNote = graphene.Field(Notes, id=graphene.Int())
+    # get all notes by user
+    user_notes = graphene.List(Notes)
+
+    def resolve_user_notes(root, info):
+        # find user with uid from token
+        # what is happening?
+        uid = info.context['uid']
+        user = session.query(UserModel).filter_by(email = uid).first()
+        return user.notes
+    
+    def resolve_findNote(root, info, id):
+        return session.query(NotesModel).filter_by(id=id).first()
+
+
+    class PreAuthQuery(graphene.ObjectType):
+        all_users = graphene.List(User)
+
+    def resolve_all_users(root, info):
+        return session.query(UserModel).all()
+
+    auth_required_schema = graphene.Schema(query=Query, mutation=PostAuthMutation)
+    schema = graphene.Schema(query=PreAuthQuery, mutation=PreAuthMutation)
